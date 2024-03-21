6aead5a9c3c4ac1925fe3d3db898d38fcd1a0a7a
Jeremy Peros <jeremy.peros@gmail.com>
2024-03-21 14:24:56 -0700

    Added init path to main file.

--- crawler/main.py
+++ crawler/main.py
@@ -1,12 +1,14 @@
 import sys
+import os
 from typing import List, Tuple
 
 # Local modules
 from crawler import Crawler
 
+PATH = os.path.join(".", "init", "rules.ini")
 def main():
     # Starts the crawler, using cmdline
-    simpleCrawler = Crawler(sys.argv[1:])
+    simpleCrawler = Crawler(sys.argv[1:], PATH)
 
 if __name__ == "__main__":
     main()
