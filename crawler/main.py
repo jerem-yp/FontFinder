e7b51026339ef12442d76d2e62f5efc540488eef
Jeremy Peros <jeremy.peros@gmail.com>
2024-03-21 16:39:03 -0700

    Added final to enforce strict no change on global constant

--- crawler/main.py
+++ crawler/main.py
@@ -1,11 +1,11 @@
 import sys
 import os
-from typing import List, Tuple
+from typing import List, Tuple, final
 
 # Local modules
 from crawler import Crawler
 
-PATH = os.path.join(".", "init", "rules.ini")
+PATH: final = os.path.join(".", "init", "rules.ini")
 def main():
     # Starts the crawler, using cmdline
     simpleCrawler = Crawler(sys.argv[1:], PATH)
