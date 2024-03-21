02af139d7b624084eabf0ef5e888e9ad8c2437b2
Jeremy Peros <jeremy.peros@gmail.com>
2024-03-21 16:38:40 -0700

    Sets searcher to get results from file if necessary.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,7 +1,13 @@
 """ Class to manage crawls"""
-from typing import List
+from typing import List, final
 from configparser import ConfigParser
 from pathlib import Path
+
+# Local modules
+from searcher import Searcher
+
+# Macros
+MAX_STARTER_LINKS: final = 100 # Number of maximum starting links, assuming reading from a file
 
 class Crawler:
 
@@ -22,3 +28,23 @@ class Crawler:
         self.result_file = config["STORAGE"].get("results", "")
         if self.result_file == "":
             raise Exception(f"Missing 'STORAGE' section or 'STORAGE/results' is empty/DNE")
+
+        if len(domains):
+            self.searcher = Searcher(domains)
+        else:
+            previousQueue = config["STORAGE"].get("YetToCrawl", "")
+            if previousQueue == "":
+                raise Exception(f"Missing 'STORAGE/results'")
+            elif not Path(previousQueue).exists():
+                raise Exception(f"Path {previousQueue} does not exist")
+
+            toSearch = []
+            with open(previousQueue, "r") as f:
+                i = 0
+                for line in f:
+                    toSearch.append(line)
+
+            with open(previousQueue, "w") as f: # Clear file
+                pass
+
+            self.searcher = Searcher(toSearch)
